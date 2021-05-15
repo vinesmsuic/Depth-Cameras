f95d97bc3ee84b4f529a57daa9cca8053faa44df
Vines <34955859+vinesmsuic@users.noreply.github.com>
2021-05-16 00:19:18 +0800

    Update RS_imageCapture.py

--- L515/RS_imageCapture.py
+++ L515/RS_imageCapture.py
@@ -1,20 +1,25 @@
-from pyrealsense2 import pyrealsense2 as rs
+import pyrealsense2 as rs
 import numpy as np
 import math
 import time
-from cv2 import cv2
-import numpngw
+import cv2
 
-device_id = '036522072893'  # serial number of device
+device_id = 'f1061955'  # serial number of device
 # Configure streams
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_device(device_id)
 
-#see d400 datasheep for depth and rgb streaming resolution!!
-config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)  # depth
+colorwidth = 1280
+colorheight = 720
+depthwidth = 1024
+depthheight = 768
+fps = 30
 
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30) # rgb
+#see d400 datasheep for depth and rgb streaming resolution!!
+config.enable_stream(rs.stream.depth, depthwidth, depthheight, rs.format.z16, fps)  # depth
+
+config.enable_stream(rs.stream.color, colorwidth, colorheight, rs.format.bgr8, fps) # rgb
 """
 config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 63) # acceleration
 config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)  # gyro
@@ -46,6 +51,7 @@ try:
         aligned_frames = align.process(frames) 
         depth_frame = aligned_frames.get_depth_frame() 
         color_frame = aligned_frames.get_color_frame()
+        #print(depth_frame.get_distance(int(depthwidth/2), int(depthheight/2)))
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
@@ -61,26 +67,29 @@ try:
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
-        print(depth_image.shape)
-        print(color_image.shape)
+        #print(depth_image.shape)
+        #print(color_image.shape)
         """
         #print IMU
         print("accel at frame {} at time {}: {}".format(str(frame_count), str(frame_time), str(accel_frame.as_motion_frame().get_motion_data())))
         print("gyro  at frame {} at time {}: {}".format(str(frame_count), str(frame_time), str(gyro_frame.as_motion_frame().get_motion_data())))
         """
-        # Press esc or 'q' to close the image window
         
+        # Press 's' to save image
         key = cv2.waitKey(1)
-        if key & 0xFF == ord('q') or key == 27:
-            #cv2.destroyAllWindows()
-            #break
+        if key & 0xFF == ord('s'):
             cv2.imwrite(f"{i}.jpg", color_image)
             cv2.imwrite(f"d{i}.png", depth_image)
 
             print(f"colored image {i}.jpg have been saved")
-            print(depth_image)
-            print(np.nanmin(depth_image))
+            #print(depth_image)
+            #print(np.nanmin(depth_image))
+            #print(np.percentile(depth_image,50))
             i += 1
+        # Press esc or 'q' to close the image window
+        elif key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
         
 finally:
     # Stop streaming
